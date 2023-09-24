298bae21b1799617712e1baf98f8309eef55627a
payn33 <amugenp2@gmail.com>
2023-09-24 09:54:02 +0100

    feat: implement locationiq api call

---
+++ src/services/geocode.ts
@@ -0,0 +1,36 @@
+import { geocodeConfig } from "../utils/config";
+import { RequestError, handleError } from "../helpers/handleError";
+import { RequestOptions, request } from "https";
+import { geocodeResponse } from "../types";
+
+export const getCoordinates = async (
+  city: string,
+  state: string
+): Promise<geocodeResponse | { error: string }> => {
+  const { options } = geocodeConfig(city, state);
+
+  return new Promise((resolve, reject) => {
+    const req = request(options, (res) => {
+      let data = "";
+
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        try {
+          const jsonData = JSON.parse(data);
+          resolve(jsonData);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+
+    req.on("error", (e) => {
+      reject(e);
+    });
+
+    req.end();
+  });
+};
